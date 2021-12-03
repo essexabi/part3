1581fa76632587ca044adf2ef80fce17cb3029d2
xabi <essexabi@gmail.com>
2021-12-03 10:30:08 +0100

    middlewares de errores creados.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-require('dotenv').config();
+///////REQUIRE DATABASE & .ENV///////////
+require("dotenv").config();
 require("./mongo");
+////////////////////////////////////////
 
 const express = require("express");
 const app = express();
 
+/////////MIDDLEWARES IMPORT//////////////
+const notFound = require("./middleware/notFound");
+const handleError = require("./middleware/handleError");
+////////////////////////////////////////
 
+/////////////CORS IMPORT////////////////
 /* app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
@@ -30,42 +37,67 @@ app.use(express.json());
     response.writeHead(200, {'Content-Type':'application/json'});
     response.end(JSON.stringify(notes));
 })*/
+////////////////////////////////////////
 
+/////////////////CRUD//////////////////
 app.get("/", (request, response) => {
     response.send(`<h1>Hello World</h1>`);
 });
 
 app.get("/api/notes", (request, response) => {
     Note.find({}).then((notes) => {
-        response.json(notes.map( note => {
-            const { _id, __v, ...restOfNote} = note.toObject();
-            return {
-                id: _id,
-                ...restOfNote      
+        response.json(
+            notes.map((note) => {
+                const { _id, __v, ...restOfNote } = note.toObject();
+                return {
+                    id: _id,
+                    ...restOfNote,
+                };
+            })
+        );
+    });
+});
+
+app.get("/api/notes/:id", (request, response, next) => {
+    const { id } = request.params;
+    Note.findById(id)
+        .then((note) => {
+            if (note) {
+                response.json(note);
+            } else {
+                response.status(404).end();
             }
-        }));
-    });
+        })
+        .catch((err) => next(err));
 });
 
-app.get("/api/notes/:id", (request, response) => {
-    const id = request.params.id;
-    Note.findById(id).then((note) => {
-        if (note) {
-            response.json(note);
-        } else {
-            response.status(404).end();
-        }
-    });
+app.put("/api/notes/:id", (request, response, next) => {
+    const { id } = request.params;
+    const note = request.body;
+
+    const newNoteInfo = {
+        title: note.title,
+        body: note.body,
+        important: note.important,
+    };
+
+    Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
+        .then((result) => {
+            response.json(result);
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-    const id = request.params.id;
-    Note.findByIdAndDelete(id).then(() => {
-       console.log("La nota ha sido borrada");
-       response.status(301);
-    }).catch(err => {
-        console.log(err)
-    })
+app.delete("/api/notes/:id", (request, response, next) => {
+    const { id } = request.params;
+    Note.findByIdAndDelete(id)
+        .then(() => {
+            response.status(204);
+            //console.log("La nota ha sido borrada");
+        })
+        .catch((err) => next(error));
 });
 
 app.post("/api/notes", (request, response) => {
@@ -88,9 +120,14 @@ app.post("/api/notes", (request, response) => {
     newNote.save().then((savedNote) => {
         response.json(savedNote);
     });
-
-    response.status(201).json(newNote);
 });
+////////////////////////////////////////
 
+//////////MIDDLEWARES//////////////////
+app.use(notFound);
+app.use(handleError);
+//////////////////////////////////////
+
+//////////SERVER CONNECTION///////////
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
